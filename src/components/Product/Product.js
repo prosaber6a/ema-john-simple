b909c6036a6ae6f65a883c890784bd5e08d3af8b
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-05 22:17:30 +0600

    Cart functionality added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,6 +5,8 @@ import './Product.css';
 
 
 const Product = (props) => {
+    console.log(props.handleAddProduct);
+    
     const {img, name, seller, price, stock} = props.product;
     return (
         <div className="product">
@@ -18,7 +20,7 @@ const Product = (props) => {
                 <p>${price}</p>
                 <br/>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
+                <button onClick={() => props.handleAddProduct(props.product)} className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
             </div>
         </div>
     );
